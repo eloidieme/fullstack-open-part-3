9ff9581cb17d8eb537129627802ceabb9aa9d78e
Eloi Dieme <eloidieme@gmail.com>
2024-02-03 20:28:13 +0100

    step 2

--- index.js
+++ index.js
@@ -26,8 +26,16 @@ persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Welcome to the server</h1>')
+app.get('/info', (request, response) => {
+    current_date = new Date()
+    response.send(
+        `<p>
+            Phonebook has info for ${persons.length} people
+        </p>
+        <p>
+            ${current_date}
+        </p>`
+    )
 })
 
 app.get('/api/persons', (request, response) => {
